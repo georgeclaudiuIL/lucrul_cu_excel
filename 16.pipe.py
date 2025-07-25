f41a0e659a6c26ab3ed89482ca8b424d6c720c4e
georgeclaudiuIL <ibanescudodo@yahoo.com>
2025-07-25 15:17:28 +0300

    Continuare 3

---
+++ 16.pipe.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+
+# extract data
+
+def extract_user_data(file_name):
+
+    return pd.read_excel(file_name)
+
+
+# filter data
+
+def filter_users(data):
+
+    mask = (pd.Timestamp.today() - data['rental_date']) > pd.Timedelta(days=31)
+
+    return data[mask]
+
+
+# sort data
+
+def sort_data(data):
+
+    return data.sort_values(by='rental_date', ascending=True)
+
+
+# add new column
+
+def add_new_column(data):
+
+    data["overdue_days"] = (pd.Timestamp.today() - data['rental_date']).dt.days - 31
+
+    return data
+
+
+# remove columns
+
+def remove_columns(data):
+
+    data = data.drop(['address', 'gender', 'city', 'active'], axis=1)
+
+    return data
+
+
+# load data
+
+def load_data(data):
+
+    data.to_excel("overdue_users.xlsx", index=False)
